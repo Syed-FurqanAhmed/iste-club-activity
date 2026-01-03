ad9a67b81b3b1733434959da9bec85881445970a
Syed-FurqanAhmed <SyedAh633@gmail.com>
2026-01-03 21:44:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js - DO NOT COMMIT THIS FILE TO GITHUB!
+const firebaseConfig = {
+  apiKey: "AIzaSyAf1JlNjAsUGib1PpZWF6xELseRERpLE48",  // Replace with the NEW key you just created
+  authDomain: "iste-toce.firebaseapp.com",
+  projectId: "iste-toce",
+  storageBucket: "iste-toce.firebasestorage.app",
+  messagingSenderId: "918228043244",
+  appId: "1:918228043244:web:891b81cc0a80127588ecc9",
+  measurementId: "G-RWG0BCFGEJ"
+};
